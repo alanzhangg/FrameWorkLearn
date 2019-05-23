91600a13cd803ef7b1d6e16ac28dd323b1d36bb4
zhang alan <alanzhangg@foxmail.com>
2019-05-23 15:25:48 +0800

    no message

--- LearnPlayground/SwiftLearn.playground/Contents.swift
+++ LearnPlayground/SwiftLearn.playground/Contents.swift
@@ -465,6 +465,84 @@ extension Array: TextRepresentable where Element: TextRepresentable{
 let myDice = [d6]
 print(myDice.textualDescription)
 
+let initialBits: UInt8 = 0b00001111
+let invertedBits = ~initialBits
+
+let firstSixBits: UInt8 = 0b11111100
+let lastSixBits: UInt8 = 0b00111111
+let middleFourBits = firstSixBits & lastSixBits
+
+let someBits: UInt8 = 0b10110010
+let moreBits: UInt8 = 0b01011110
+let combinedBits = someBits | moreBits
+
+let firstBits: UInt8 = 0b00010100
+let otherBits: UInt8 = 0b00000101
+let outputBits = firstBits ^ otherBits
+
+let shiftBits: UInt8 = 255
+shiftBits << 1
+shiftBits << 2
+shiftBits << 5
+shiftBits << 6
+shiftBits >> 2
+
+let pink : UInt32 = 0xcc6699
+let redComponent = (pink & 0xff0000) >> 16
+let greenComponent = (pink & 0x00ff00) >> 8
+let blueComponent = (pink & 0x0000ff)
+
+var unsignedOverflow = UInt8.min
+unsignedOverflow = unsignedOverflow &- 1
+
+
+struct Vector2D{
+    var x = 0.0, y = 0.0
+}
+
+extension Vector2D{
+    static func + (left: Vector2D, right: Vector2D) -> Vector2D{
+        return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+extension Vector2D{
+    static prefix func - (vector: Vector2D) -> Vector2D{
+        return Vector2D(x: -vecctor.x, y: -vecctor.y)
+    }
+}
+
+extension Vector2D{
+    static func += (left: inout Vector2D, right: Vector2D){
+        left = left + right
+    }
+}
+
+let vecctor = Vector2D(x: 3.0, y: 1.0)
+let anotherVector = Vector2D(x: 2.0, y: 4.0)
+let combinedVector = vecctor + anotherVector
+let negative = -vecctor
+
+prefix operator +++
+extension Vector2D {
+    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+
+infix operator +-: AdditionPrecedence
+extension Vector2D {
+    static func +- (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y - right.y)
+    }
+}
+let firstVector = Vector2D(x: 1.0, y: 2.0)
+let secondVector = Vector2D(x: 3.0, y: 4.0)
+let plusMinusVector = firstVector +- secondVector
+
+var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+let afterDoubling = +++toBeDoubled
 
 
 
